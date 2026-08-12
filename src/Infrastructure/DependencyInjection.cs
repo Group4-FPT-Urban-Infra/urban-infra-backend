a01049335dd7b5234f5e3eb8a03a0f9399169357
Phung Hai Nam <nam0072004@gmail.com>
2026-08-12 10:21:34 +0700

    [BE] Background job kiểm tra Issue_SLAs định kỳ

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -85,6 +85,9 @@ public static class DependencyInjection
         services.AddScoped<IIssueService, IssueService>();
         services.AddScoped<IRoutingRuleService, RoutingRuleService>();
 
+        // --- Background Hosted Services ---
+        services.AddHostedService<UrbanInfraSystem.Infrastructure.BackgroundServices.SlaCheckBackgroundService>();
+
         return services;
     }
 }
